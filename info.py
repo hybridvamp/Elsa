a5c4dd77a4050fbc66a0890954c8c428f1d53ddc
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2024-05-07 07:13:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "GoYoonJung"")
+DATABASE_NAME = environ.get('DATABASE_NAME', "GoYoonJung")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 MONGO_URL = os.environ.get('MONGO_URL', "")
 
